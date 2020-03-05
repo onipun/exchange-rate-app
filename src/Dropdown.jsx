b9d1ceb7b8ce12e34c750385d74a76d679bf2a3b
shaufyq <shaufyq@aleph-labs.com>
2020-03-05 21:36:15 +0800

    fix euro currency code

--- src/Dropdown.jsx
+++ src/Dropdown.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import { Dropdown } from 'semantic-ui-react'
 import PropTypes from 'prop-types';
 export const countryOptions = [
-  { key: 'EURO', value: 'EURO', flag: 'ro', text: 'European' },
+  { key: 'EUR', value: 'EUR', flag: 'ro', text: 'European' },
   { key: 'USD', value: 'USD', flag: 'us', text: 'United State' },
   { key: 'JPY', value: 'JPY', flag: 'an', text: 'Japan' },
   { key: 'MYR', value: 'MYR', flag: 'my', text: 'Malaysia' },
